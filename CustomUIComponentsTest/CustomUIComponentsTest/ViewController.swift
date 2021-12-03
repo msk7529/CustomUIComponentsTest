3efaf17c28d85862669c4c3de660593cad80ac3f
G.magpie <msk7529@naver.com>
2021-12-03 17:17:54 +0900

    프로젝트 생성

---
+++ CustomUIComponentsTest/CustomUIComponentsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomUIComponentsTest
+//
+//  Created on 2021/12/03.
+//
+
+import SnapKit
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CustomUIComponentsTest/Pods/Target Support Files/Pods-CustomUIComponentsTest/Pods-CustomUIComponentsTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomUIComponentsTest : NSObject
+@end
+@implementation PodsDummy_Pods_CustomUIComponentsTest
+@end
